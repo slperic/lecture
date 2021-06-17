c8e3c6da28c5bbdf9d9d10670b6fcf9850f9ec07
slperic <slperic6278@gmail.com>
2021-06-17 12:43:30 +0900

    add amugeona

--- node/client.js
+++ node/client.js
@@ -1,15 +1,23 @@
+const { TIMEOUT } = require('dns');
 var net = require('net');
+//net이라는 내장모듈을 사용할 거임
 function getConnection(connName){
-  var client = net.connect({port: 8107, host:'172.28.94.218'}, function() {
-    this.setTimeout(500);
-    this.setEncoding('utf8');
-    this.on('data', function(data) {
-      this.end();
+  var client = net.connect({port: 8107, host:'172.24.37.25'}, function() {
+    console.log(connName)
+    this.setTimeout(500);//5초동안 쉼 
+    this.setEncoding('utf8');//인코딩 형식 정해줬음, base64도 많이 쓰임
+    this.on('data', function(data) { //data라는 게 오면 on 이벤트가 발생
+        console.log(data)
+        this.end();
     });
-    this.on('end', function() {
+    this.on('end', function() {//this=end 가 오면 어떤 기능이 생성될지
     });
     this.on('error', function(err) {
+        console.log(err)
     });
+//     this.on('error', (err) => { //화살표 함수!
+//         console.log(err)
+//     });
     this.on('timeout', function() {
     });
     this.on('close', function() {
--- node/server.js
+++ node/server.js
@@ -1,10 +1,13 @@
 
 var net = require('net');
 var server = net.createServer(function(client) {
-  client.setTimeout(500);
-  client.setEncoding('utf8');
-  client.on('data', function(data) {
-    writeData(client, 'Sending: ' + data.toString());
+    //createServer를 쓸 줄 알면 됐음
+    console.log("hello world")
+    client.setTimeout(500);
+    client.setEncoding('utf8');
+    client.on('data', function(data) {
+        username=data.split("|")[0]
+        writeData(client, 'Sending : nice to meet you '+username);
   });
   client.on('end', function() {
     server.getConnections(function(err, count){
@@ -20,9 +23,10 @@ server.listen(8107, function() {
   server.on('close', function(){
   });
   server.on('error', function(err){
+    console.log(err)
   });
 });
-function writeData(socket, data){
+function writeData(socket, data){ //어떤 소켓을 받아 데이터를 쓴다
   var success = !socket.write(data);
   if (!success){
     (function(socket, data){
